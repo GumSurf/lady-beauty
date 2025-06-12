0502ce8fc299d7fc36d3a3be891947d14333b843
GumSurf <gabriel.christe@gmail.com>
2025-06-13 01:26:47 +0200

    Add Helmet pour un meilleur Seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -9,35 +10,34 @@ import Contact from "./pages/Contact";
 
 // Import des pages services
 import SoinVisageCorps from "./pages/services/SoinVisageCorps";
-import Microbladinge from "./pages/services/Microblading";
+import Microblading from "./pages/services/Microblading";
 import Microneedling from "./pages/services/Microneedling";
 import Ventouse from "./pages/services/VentouseTherapy";
-import Plasmapene from "./pages/services/PlasmaPen";
+import Plasmapen from "./pages/services/PlasmaPen";
 import Anticellulite from "./pages/services/Anticellulite";
 import EpilationCireFil from "./pages/services/Epilation";
+import Service from "./pages/services/[slug]";
 
 
 function App() {
     return (
-        <div className="bg-pink-50 text-gray-800 min-h-screen flex flex-col">
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/services" element={<ServicesList />} />
-                    <Route path="/booking" element={<Booking />} />
-                    <Route path="/contact" element={<Contact />} />
+        <HelmetProvider>
+            <div className="bg-pink-50 text-gray-800 min-h-screen flex flex-col">
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/services" element={<ServicesList />} />
+                        <Route path="/booking" element={<Booking />} />
+                        <Route path="/contact" element={<Contact />} />
 
-                    {/* Routes services individuelles */}
-                    <Route path="/services/soin-visage-corps" element={<SoinVisageCorps />} />
-                    <Route path="/services/microblading" element={<Microbladinge />} />
-                    <Route path="/services/microneedling" element={<Microneedling />} />
-                    <Route path="/services/ventouse" element={<Ventouse />} />
-                    <Route path="/services/plasmapene" element={<Plasmapene />} />
-                    <Route path="/services/anticellulite" element={<Anticellulite />} />
-                    <Route path="/services/epilation-cire-fil" element={<EpilationCireFil />} />
-                </Routes>
-            </Layout>
-        </div>
+                        {/* Routes services individuelles */}
+                        <Route path="/services/anticellulite" element={<Anticellulite />} />
+                        <Route path="/services/epilation-cire-fil" element={<EpilationCireFil />} />
+                        <Route path="/services/:slug" element={<Service />} />
+                    </Routes>
+                </Layout>
+            </div>
+        </HelmetProvider>
     );
 }
 
@@ -54,58 +54,3 @@ function Layout({ children }) {
 }
 
 export default App;
-
-/*import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import ServicesList from "./pages/ServicesList";
-import Booking from "./pages/Booking";
-import Contact from "./pages/Contact";
-
-// Import des pages services adaptées
-import SoinVisageCorps from "./pages/services/SoinVisageCorps";
-import Microbladinge from "./pages/services/Microblading";
-import Microneedling from "./pages/services/Microneedling";
-import Ventouse from "./pages/services/VentouseTherapy";
-import Plasmapene from "./pages/services/PlasmaPen";
-import Dermapene from "./pages/services/Dermapen";
-import Anticellulite from "./pages/services/Anticellulite";
-import EpilationCireFil from "./pages/services/Epilation";
-
-function App() {
-    return (
-        <div className="bg-pink-50 text-gray-800 min-h-screen flex flex-col">
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/services" element={<ServicesList />} />
-                    <Route path="/booking" element={<Booking />} />
-                    <Route path="/contact" element={<Contact />} />
-
-                    <Route path="/services/soin-visage-corps" element={<SoinVisageCorps />} />
-                    <Route path="/services/microblading" element={<Microbladinge />} />
-                    <Route path="/services/microneedling" element={<Microneedling />} />
-                    <Route path="/services/ventouse" element={<Ventouse />} />
-                    <Route path="/services/plasmapene" element={<Plasmapene />} />
-                    <Route path="/services/dermapene" element={<Dermapene />} />
-                    <Route path="/services/anticellulite" element={<Anticellulite />} />
-                    <Route path="/services/epilation-cire-fil" element={<EpilationCireFil />} />
-                </Routes>
-            </Layout>
-        </div>
-    );
-}
-
-function Layout({ children }) {
-    return (
-        <>
-            <Navbar />
-            <main className="pt-16">{children}</main>
-            <Footer />
-        </>
-    );
-}
-
-export default App;*/

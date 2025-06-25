299266b289add9ce04036df7854b850d557ebd0d
GumSurf <gabriel.christe@gmail.com>
2025-06-25 15:15:41 +0200

    Add: better optimization

--- src/components/OptimizedImage.jsx
+++ src/components/OptimizedImage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const OptimizedImage = ({
-  basePath, // ex: "/images/massage"
+  basePath,
   alt = "",
   className = "",
   loading = "lazy",
   type = "webp",
-  fallbackExtension = "jpg", // ou "png"
-  sizes = "(max-width: 768px) 100vw, 50vw",
+  fallbackExtension = "jpg",
+  sizes="50vw"
+
 }) => {
   const srcSet = [480, 768, 1280, 1920]
     .map((size) => `${basePath}-${size}.${type} ${size}w`)
